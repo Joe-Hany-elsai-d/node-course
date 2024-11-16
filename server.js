2163dadad06c7a612665d04191000162f5d70391
youssef <youssef@email.com>
2024-11-16 11:00:09 +0200

    using the environment variable as the port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const hbs = require("hbs");
 const app = express();
 
+const port = process.env.PORT || 3000; 
 // app.use(express.static("public"));
 
 // app.listen(3000);
@@ -23,7 +24,7 @@ app.get("/about", (req, res) => {
    });
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("The server is running on port 3000");
     
 })
