687557e2411111cf96dde60fac65aa019c4eaa46
youssef <youssef@email.com>
2024-11-16 10:09:25 +0200

    updating the .gitignore file

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+
+// app.use(express.static("public"));
+
+// app.listen(3000);
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerPartials(__dirname + "/views/partials")
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.get("/about", (req, res) => {
+   res.render("about", {
+    userName: "Joe", 
+    age: 15,
+   });
+})
+
+app.listen(3000, () => {
+    console.log("The server is running on port 3000");
+    
+})
